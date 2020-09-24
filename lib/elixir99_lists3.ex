36432936d6eb62bbef93a6159b82f2b0dc789620
unknown <arturkasza98@gmail.com>
2020-09-24 10:24:40 +0200

    Further solutions and test

--- lib/elixir99_lists3.ex
+++ lib/elixir99_lists3.ex
@@ -11,4 +11,31 @@ defmodule Elixir99.Lists3 do
       false -> range(s+1, e, acc++[s])
     end
   end
+  def rnd_select(list, n, acc \\ [])
+  def rnd_select(list, 1, acc) do
+    index = length(list)
+                    |> :rand.uniform
+                    |> Kernel.-(1)
+    picked = list |> Elixir99.Lists.element_at(index)
+
+    case acc do
+      [] -> {[picked], index}
+      _ ->
+        acc++[picked]
+    end
+  end
+
+  def rnd_select(list, n, acc) do
+    {picked, index} = rnd_select(list, 1)
+    new_list = Elixir99.Lists2.remove_at(list, index+1) |> elem(1)
+    rnd_select(new_list, n-1, acc++picked)
+  end
+
+  def lotto(n, m) do
+    rnd_select(range(1, m), n, [])
+  end
+
+  def rnd_permu(list) do
+    rnd_select(list, length(list))
+  end
 end
--- test/elixir99_lists3_test.exs
+++ test/elixir99_lists3_test.exs
@@ -10,4 +10,29 @@ defmodule Elixir99.Lists3Test do
   test "range should create a list of numbers within a given range" do
     assert range(4, 9) == Enum.to_list 4..9
   end
+
+  test "rnd_select should return n random elements from given list (without repetitions)" do
+    n = 3
+    list = 'abcdefgh'
+    picked = list |> rnd_select(n)
+    assert picked |> Enum.all?(&(Enum.member?(list, &1)))
+    assert length(picked) == n
+    assert picked |> Enum.uniq |> length == n
+  end
+
+  test "lotto should pick N different numbers from set 1..M" do
+    n = 3
+    m = 49
+    picked = lotto(n, m)
+    assert length(picked) == n
+    assert picked |> Enum.all?(&(Enum.member?(1..m, &1)))
+    assert picked |> Enum.uniq |> length == n
+  end
+
+  test "rnd_permu should generate radnom permutation of list's elements (shuffle it)" do
+    l1 = 'abcdefgh'
+    l2 = [1, 2, 3, 4, 5]
+    assert l1 |> rnd_permu |> Enum.sort == l1
+    assert l2 |> rnd_permu |> Enum.sort == l2
+  end
 end
